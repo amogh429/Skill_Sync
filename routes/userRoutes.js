7923a7300a3d6583c6f8686fbd87c809fe196612
amogh429 <amogh.allrounder@gmail.com>
2026-03-28 21:51:54 +0530

    feat: add user getProfile,updateProfile,getAllUsers,getUsersById

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { getMyProfile } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
+import { getMyProfile, updateProfile , getAllUsers , getUserById } from '../controllers/userController.js';
+
 
 const router = express.Router();
 
 // Important :- protect comes BEFORE controller
 router.get('/profile', protect, getMyProfile);
+
+router.put('/profile',protect,updateProfile);
+
+router.get('/',protect,getAllUsers);
+
+router.get('/:id',protect,getUserById);
 
 export default router;
