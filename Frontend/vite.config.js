34a1816059624b04c67e4bd70fc99c9800a0c3df
amogh429 <amogh.allrounder@gmail.com>
2026-05-08 21:44:26 +0530

    feat: build tailiwind css

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import { fileURLToPath } from "url"
+import tailwindcss from '@tailwindcss/vite'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 
 export default defineConfig({
    server: {
@@ -12,7 +14,10 @@ export default defineConfig({
       "/api": "http://localhost:5000",
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
