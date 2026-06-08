c99c3f1b0d2b83572688683e4f9c5a1cc96061a4
amogh429 <amogh.allrounder@gmail.com>
2026-06-08 20:56:37 +0530

    feat: Fix build issue

--- Frontend/src/pages/ConnectionsPage.jsx
+++ Frontend/src/pages/ConnectionsPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "../api/axios";
 import { useAuth } from "../context/useAuth";
-import Navbar from "../components/shared/Navbar";
+import Navbar from "../components/Shared/Navbar";
 
 const ConnectionsPage = () => {
   const [connections, setConnections] = useState([]);
